edf7964569040944db0eff9502d57681e68eec42
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-03-11 09:45:36 +0500

    Button Ripple

---
+++ 20-Button Ripple Effect/script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", (e) => {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  const buttonTop = e.target.offsetTop;
+  const buttonLeft = e.target.offsetLeft;
+  const xPos = x - buttonLeft;
+  const yPos = y - buttonTop;
+
+  btn.innerHTML = `Click Me <span class="circle" style="left: ${xPos}px; top: ${yPos}px"></span>`;
+});
