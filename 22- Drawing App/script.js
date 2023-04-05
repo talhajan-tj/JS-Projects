3c0f4a232a5792188619c957df7e391db85f287d
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-04-05 23:24:18 +0500

    Drawin App

--- 22- Drawing App/script.js
+++ 22- Drawing App/script.js
@@ -3,10 +3,38 @@ const ctx = canvas.getContext("2d");
 
 let size = 20;
 let color = "black";
+let p1, p2;
+let isPressed = false;
 
-function drawCiecle(x, y) {
+canvas.addEventListener("mousedown", (e) => {
+  isPressed = true;
+  p1 = e.offsetX;
+  p2 = e.offsetY;
+  console.log(p1, p2);
+});
+
+canvas.addEventListener("mouseup", (e) => {
+  isPressed = false;
+  p1 = undefined;
+  p2 = undefined;
+  console.log(p1, p2);
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (isPressed) {
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    drawCiecle(x, y);
+    drawLine(p1, p2, x, y);
+    p1 = x;
+    p2 = y;
+  }
+});
+
+function drawCiecle(p1, p2) {
   ctx.beginPath();
-  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.arc(p1, p2, size, 0, Math.PI * 2);
   ctx.fillStyle = color;
   ctx.fill();
 }
@@ -16,9 +44,6 @@ function drawLine(p1, p2, x, y) {
   ctx.moveTo(p1, p2);
   ctx.lineTo(x, y);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
-
-drawCiecle(200, 200);
-drawLine(400, 400, 400, 800);
