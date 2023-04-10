5ff954f5ed4f8307c3924d271dc36515b78721d1
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-04-10 23:05:44 +0500

    Notification Popup

--- 24- Notification Popup/script.js
+++ 24- Notification Popup/script.js
@@ -2,12 +2,17 @@ const btn = document.getElementById("button");
 const toast = document.getElementById("toast");
 const msgs = ["Message One", "Message Two", "Message Three", "Message Four"];
 
-btn.addEventListener("click", () => {
+btn.addEventListener("click", () => createNotification());
+
+function createNotification() {
   const alert = document.createElement("div");
   alert.classList.add("toast");
   alert.innerText = randomMsg();
   toast.appendChild(alert);
-});
+  setTimeout(() => {
+    alert.remove();
+  }, 3000);
+}
 
 function randomMsg() {
   return msgs[Math.floor(Math.random() * msgs.length)];
