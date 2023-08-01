2dedcbf5478525e3335cb2af2e9c77d5f07914e3
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-01 14:33:09 +0500

    Content Placeholder

--- 25-Content Placeholder/script.js
+++ 25-Content Placeholder/script.js
@@ -1,5 +1,13 @@
 const card = document.querySelector(".card");
 
-setTimeout(() => {
+setInterval(() => {
   card.classList.remove("animate-active");
-}, 10000);
+  afterAnimate();
+}, 8000);
+
+function afterAnimate() {
+  const cardImg = document.querySelector(".img img");
+
+  cardImg.src = "laptop.jpg";
+  cardImg.style.opacity = "1";
+}
