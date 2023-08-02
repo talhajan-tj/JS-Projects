f9c2c273347c1029bd5f028f6d4cc06a8558ca46
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-02 19:40:42 +0500

    Sticky Navbar

---
+++ 26-sticky Navbar/script.js
@@ -0,0 +1,16 @@
+let scrollpos = window.scrollY;
+const header = document.querySelector("header");
+const header_height = header.offsetHeight;
+
+const add_class_on_scroll = () => header.classList.add("active");
+const remove_class_on_scroll = () => header.classList.remove("active");
+
+window.addEventListener("scroll", function () {
+  scrollpos = window.scrollY;
+
+  if (scrollpos >= header_height) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
+});
