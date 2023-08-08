c0f9947158a0bfb96ebadf829cfa7ea702a0cb73
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-08 12:59:09 +0500

    Vertical Slider

---
+++ 27-Vertical Slider/script.js
@@ -0,0 +1,6 @@
+const slider = document.querySelector(".slider");
+const slideLeft = document.querySelector(".slide-left");
+const slideRight = document.querySelector(".slide-right");
+const downBtn = document.querySelector(".down-btn");
+const upBtn = document.querySelector(".up-btn");
+const sliderLength = slideRight.querySelectorAll("div").length;
