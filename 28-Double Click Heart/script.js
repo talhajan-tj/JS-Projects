4562dcdd6778d67bae327ef9f8704c00738eea7b
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-15 09:19:42 +0500

    Double Click Heart

--- 28-Double Click Heart/script.js
+++ 28-Double Click Heart/script.js
@@ -1,32 +1,13 @@
-const slider = document.querySelector(".slider");
-const slideLeft = document.querySelector(".slide-left");
-const slideRight = document.querySelector(".slide-right");
-const downBtn = document.querySelector(".down-btn");
-const upBtn = document.querySelector(".up-btn");
-const sliderLength = slideRight.querySelectorAll("div").length;
+const img = document.querySelector(".box img");
+const likes = document.querySelector(".likes");
+const heart = document.querySelector(".heart");
 
-let activeSlideIndex = 0;
+let likesCount = 0;
 
-slideLeft.style.top = `-${(sliderLength - 1) * 100}vh`;
-
-upBtn.addEventListener("click", () => changeSlide("up"));
-downBtn.addEventListener("click", () => changeSlide("down"));
-
-const changeSlide = (direction) => {
-  const slideHeight = slider.clientHeight;
-  if (direction === "up") {
-    activeSlideIndex++;
-    if (activeSlideIndex > sliderLength - 1) {
-      activeSlideIndex = 0;
-    }
-  } else if (direction === "down") {
-    activeSlideIndex--;
-    if (activeSlideIndex < 0) {
-      activeSlideIndex = sliderLength - 1;
-    }
-  }
-  slideRight.style.transform = `translateY(-${
-    activeSlideIndex * slideHeight
-  }px)`;
-  slideLeft.style.transform = `translateY(${activeSlideIndex * slideHeight}px)`;
-};
+img.addEventListener("dblclick", () => {
+  likes.innerText = ++likesCount;
+  heart.classList.add("clicked");
+  setTimeout(() => {
+    heart.classList.remove("clicked");
+  }, 1000);
+});
