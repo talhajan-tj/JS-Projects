44afc40392533bb51c2ea1819756429cd84c07b0
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-16 08:03:42 +0500

    Double Click Heart

--- 28-Double Click Heart/script.js
+++ 28-Double Click Heart/script.js
@@ -4,10 +4,17 @@ const heart = document.querySelector(".heart");
 
 let likesCount = 0;
 
-img.addEventListener("dblclick", () => {
+img.addEventListener("dblclick", (e) => {
   likes.innerText = ++likesCount;
   heart.classList.add("clicked");
   setTimeout(() => {
     heart.classList.remove("clicked");
   }, 1000);
+
+  const x = e.clientX;
+  const y = e.clientY;
+  console.log(x, y);
+  const offsetLeft = e.target.offsetLeft;
+  const offsetTop = e.target.offsetTop;
+  console.log(offsetLeft, offsetTop);
 });
