f7f73bc5d78c1a159709654c95a56621cc50e0e5
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-08-16 09:04:38 +0500

    Double Click Heart

--- 28-Double Click Heart/script.js
+++ 28-Double Click Heart/script.js
@@ -1,20 +1,33 @@
 const img = document.querySelector(".box img");
 const likes = document.querySelector(".likes");
 const heart = document.querySelector(".heart");
+const heartIcon = document.querySelector(".fa-heart");
 
 let likesCount = 0;
 
 img.addEventListener("dblclick", (e) => {
+  console.log(e);
   likes.innerText = ++likesCount;
   heart.classList.add("clicked");
   setTimeout(() => {
     heart.classList.remove("clicked");
   }, 1000);
+  heartPos(e);
+});
 
+const heartPos = (e) => {
   const x = e.clientX;
   const y = e.clientY;
   console.log(x, y);
-  const offsetLeft = e.target.offsetLeft;
-  const offsetTop = e.target.offsetTop;
+
+  const offsetLeft = e.offsetX;
+  const offsetTop = e.offsetY;
   console.log(offsetLeft, offsetTop);
-});
+
+  heartIcon.style.top = `${offsetLeft}px`;
+  heartIcon.style.left = `${offsetTop}px`;
+  heartIcon.classList.add("active");
+  setTimeout(() => {
+    heartIcon.classList.remove("active");
+  }, 1000);
+};
