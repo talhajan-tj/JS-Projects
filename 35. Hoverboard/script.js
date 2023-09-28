b3989680f985d5372e031fc525eee0037fc5e9b1
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-09-28 19:24:23 +0500

    HoverBoard final

--- 35. Hoverboard/script.js
+++ 35. Hoverboard/script.js
@@ -6,5 +6,21 @@ const squareNum = 500;
 for (let i = 0; i < squareNum; i++) {
   const square = document.createElement("div");
   square.classList.add("square");
+  square.addEventListener("mouseover", () => addColor(square));
+  square.addEventListener("mouseout", () => removeColor(square));
   hoverboard.appendChild(square);
+}
+
+function addColor(el) {
+  const color = getRandomColor();
+  el.style.background = color;
+  el.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+function removeColor(el) {
+  el.style.background = "#1d1d1d";
+  el.style.boxShadow = "0 0 2px #000";
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
 }
