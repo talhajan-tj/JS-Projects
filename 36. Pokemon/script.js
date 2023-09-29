21bb219d90c96f742ca28267ac8ef30f3ea3c354
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-09-29 17:04:51 +0500

    Pokemon (uncomplete)

---
+++ 36. Pokemon/script.js
@@ -0,0 +1,37 @@
+// This project is uncomplete because of API issue
+
+const poke_container = document.getElementById("pokedex-container");
+
+const poke_count = 150;
+
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+};
+
+const fetchPokemons = async () => {
+  for (let i = 0; i < poke_count; i++) {
+    await getPokemon(i);
+  }
+};
+
+const getPokemon = async (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data);
+};
+
+fetchPokemons();
