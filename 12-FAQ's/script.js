51ed0f33c2cd152b93f5132d76920a06ce37779e
Muhammad Talha Jan <122998881+talhajan-tj@users.noreply.github.com>
2023-10-06 20:01:11 +0500

    Update script.js

--- 12-FAQ's/script.js
+++ 12-FAQ's/script.js
@@ -5,3 +5,16 @@ toggles.forEach((toggle) => {
     toggle.parentNode.classList.toggle("active");
   });
 });
+
+// const toggles = document.querySelectorAll(".toggle");
+// const heading = document.querySelector(".faq h3");
+// const faq = document.querySelector(".faq");
+// toggles.forEach((toggle) => {
+//   toggle.addEventListener("click", () => {
+//     toggle.parentNode.classList.toggle("active");
+//   });
+// });
+// heading.addEventListener("click", () => {
+//   console.log("heading clicked");
+//   faq.classList.toggle("active");
+// });
